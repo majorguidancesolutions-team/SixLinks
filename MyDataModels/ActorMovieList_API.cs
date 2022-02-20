ffcd16128ce9aabc21fd41068983e4225c7ebe28
yentcour <courtney.henshaw1@outlook.com>
2022-02-20 10:37:13 -0500

    autoformatted all files and moved imdb classes to mydatamodels

--- MyDataModels/ActorMovieList_API.cs
+++ MyDataModels/ActorMovieList_API.cs
@@ -6,38 +6,38 @@ using System.Threading.Tasks;
 
 namespace MyDataModels
 {
-	public class ActorMovieList_API
-	{
-		public string id { get; set; }
-		public string name { get; set; }
-		public string role { get; set; }
-		public string image { get; set; }
-		public string summary { get; set; }
-		public string birthDate { get; set; }
-		public object deathDate { get; set; }
-		public string awards { get; set; }
-		public string height { get; set; }
-		public List<KnownFor> knownFor { get; set; }
-		public List<CastMovy> castMovies { get; set; }
-		public string errorMessage { get; set; }
-	}
+    public class ActorMovieList_API
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string role { get; set; }
+        public string image { get; set; }
+        public string summary { get; set; }
+        public string birthDate { get; set; }
+        public object deathDate { get; set; }
+        public string awards { get; set; }
+        public string height { get; set; }
+        public List<KnownFor> knownFor { get; set; }
+        public List<CastMovy> castMovies { get; set; }
+        public string errorMessage { get; set; }
+    }
 
-	public class CastMovy
-	{
-		public string id { get; set; }
-		public string role { get; set; }
-		public string title { get; set; }
-		public string year { get; set; }
-		public string description { get; set; }
-	}
+    public class CastMovy
+    {
+        public string id { get; set; }
+        public string role { get; set; }
+        public string title { get; set; }
+        public string year { get; set; }
+        public string description { get; set; }
+    }
 
-	public class KnownFor
-	{
-		public string id { get; set; }
-		public string title { get; set; }
-		public string fullTitle { get; set; }
-		public string year { get; set; }
-		public string role { get; set; }
-		public string image { get; set; }
-	}
+    public class KnownFor
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string fullTitle { get; set; }
+        public string year { get; set; }
+        public string role { get; set; }
+        public string image { get; set; }
+    }
 }
